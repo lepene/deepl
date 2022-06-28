81fdd11985014b8e1e470a375a59d659d977b600
lepene@gmail.com <lepene@gmail.com>
2022-06-28 03:43:10 +0200

    ok

---
+++ venv/src/main/python/main_window.py
@@ -0,0 +1,8 @@
+from PySide2 import QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle("Deepl")
+
