f06d9a0864ce1a6b50e613f4bcc001b017ac00e9
lepene@gmail.com <lepene@gmail.com>
2022-06-28 03:39:31 +0200

    ok

--- venv/src/main/python/main.py
+++ venv/src/main/python/main.py
@@ -1,15 +1,21 @@
 from fbs_runtime.application_context.PySide2 import ApplicationContext
-from PySide2.QtWidgets import QMainWindow
-import Deepl
+from main_window import MainWindow
+import deepl
 import sys
 
 if __name__ == '__main__':
-    deepl = Deepl
+    with open("deepl_auth_key.txt") as f:
+        auth_key = f.readlines()[0]
+        if len(auth_key) == 36:
+            translator = deepl.Translator(auth_key)
+        if bool(translator):
+            result = translator.translate_text("Hello, world!", target_lang="FR")
+            print(result.text)
 
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
+    window = MainWindow()
     window.resize(250, 150)
-    window.setWindowTitle("Deepl")
+
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
